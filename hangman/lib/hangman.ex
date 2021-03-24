30b6957401da1ebb73adb497d8724cf9152b99e3
Alena Gilevskaya <alena@gilevskaya.com>
2021-03-23 22:37:04 -0700

    base game

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,6 +1,11 @@
 defmodule Hangman do
-  def hello do
-    Dictionary.random_word() |> IO.puts()
+  alias Hangman.Game
+  defdelegate new_game(), to: Game
+  defdelegate tally(game), to: Game
+
+  def make_move(game, guess) do
+    game = Game.make_move(game, guess)
+    {game, tally(game)}
   end
 end
 
